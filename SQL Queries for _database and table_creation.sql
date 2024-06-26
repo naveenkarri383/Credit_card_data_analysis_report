200f8a9e241997ddfc8885bb1ffb3b82d6928715
naveenkarri383 <karrinaveen.mme@gmail.com>
2024-06-26 21:53:22 +0530

    data added to MySQL database

---
+++ SQL Queries for _database and table_creation.sql
@@ -0,0 +1,51 @@
+-- Create the database
+CREATE DATABASE credit_card_details;
+-- Go to the newly created database
+USE credit_card_details;
+-- Create the table with the data of credit card information
+CREATE TABLE data_credit_card_info (
+    Client_Num INT,
+    Card_Category VARCHAR(20),
+    Annual_Fees INT,
+    Activation_30_Days INT,
+    Customer_Acq_Cost INT,
+    Week_Start_Date DATE,
+    Week_Num VARCHAR(20),
+    Qtr VARCHAR(10),
+    current_year INT,
+    Credit_Limit DECIMAL(10,2),
+    Total_Revolving_Bal INT,
+    Total_Trans_Amt INT,
+    Total_Trans_Ct INT,
+    Avg_Utilization_Ratio DECIMAL(10,3),
+    Use_Chip VARCHAR(10),
+    Exp_Type VARCHAR(50),
+    Interest_Earned DECIMAL(10,3),
+    Delinquent_Acc VARCHAR(5)
+);
+
+ -- Create the table with the data of customer details
+CREATE TABLE customer_details (
+    Client_Num INT,
+    Customer_Age INT,
+    Gender VARCHAR(5),
+    Dependent_Count INT,
+    Education_Level VARCHAR(50),
+    Marital_Status VARCHAR(20),
+    State_cd VARCHAR(50),
+    Zipcode VARCHAR(20),
+    Car_Owner VARCHAR(5),
+    House_Owner VARCHAR(5),
+    Personal_Loan VARCHAR(5),
+    Contact VARCHAR(50),
+    Customer_Job VARCHAR(50),
+    Income INT,
+    Cust_Satisfaction_Score INT
+);
+-- SQL queries to retrieve data from the tables
+SHOW DATABASES;
+USE credit_card_details;
+SHOW TABLES;
+SELECT * FROM data_credit_card_info;
+SELECT * FROM customer_details;
+
